ecea9bfad84ae8f458c5a53b0b338c93c19d0a84
Alexandre Felipe <o-alexandre-felipe@bitbucket.org>
2019-01-17 20:16:10 -0200

    bug fixes

--- examples/event_server.py
+++ examples/event_server.py
@@ -1,63 +1,5 @@
-import threading;
-import bisect;
 from websocket_server import WebsocketServer, WebsocketClientBehavior
-
-class EventSource:
-  def __init__(self):
-    self.priorityList = list();
-    self.idList = list();
-    self.handlerList = list();
-    self.id_counter = 0;
-    self._lock = threading.Lock();
-  def addListener(self, handler, priority):
-    #  O(log(n)) - based on the priority list that is
-    #              necessarily sorted it can be determined
-    #              efficiently the insertion point
-    with self._lock:
-      self.id_counter += 1;
-      i = bisect.bisect_right(self.priorityList, priority);
-      self.priorityList.insert(i, priority);
-      self.idList.insert(i, self.id_counter);
-      self.handlerList.insert(i, handler);
-      return self.id_counter;
-
-  def removeListener(self, id):
-    # O(n) - the identification list is not necessarily sorted
-    #        and all the listeners may have the same priority
-    with self._lock:
-      i = self.idList.find(id);
-      if len(self.idList) != 0:
-        if self.idList[i] == i:
-          del self.priorityList[i];
-          del self.idList[i];
-          del self.handlerList[i];
-  def notify(self, data):
-    # O(1) - the listener with highest priority is at the beginning of the 
-    # list, so it can be called readily
-    with self._lock:
-      if len(self.handlerList) == 0:
-        return;
-      fn = self.handlerList[-1]
-    return fn(data);
-
-
-
-class EventSourceManager:
-  def __init__(self):
-    self.eventSources = dict();
-  def addEventSource(self, ev, idSource):
-    self.eventSources[idSource] = ev;
-  def removeEventSource(self, idSource):
-    if idSource in self.eventSources:
-      del self.eventSources[idSource]
-  def connect(self, idSource, handler, priority):
-    if idSource in self.eventSources:
-      return self.eventSources[idSource].addListener(handler, priority);
-  def disconnect(self, idSource, idListener):
-    if idSource in self.eventSources:
-      return eventSources[idSource].removeListener(self, idListener);
-
-
+from websocket_server import EventSource, EventSourceManager
 
 class senderBhv(WebsocketClientBehavior):
   '''
@@ -100,8 +42,9 @@ class listenerBhv(WebsocketClientBehavior):
       self.send_text(str(e));
       return
     if(idSource in self.eventManager.eventSources):
+      key = self.eventManager.connect(idSource, self.notify_event, priority);
       self.events.append(
-         (idSource, self.eventManager.connect(idSource, self.notify_event, priority))
+         (idSource, key)
       );
       self.send_text('listening to events from ' + str(idSource));
     else:
@@ -109,8 +52,8 @@ class listenerBhv(WebsocketClientBehavior):
   def notify_event(self, data):
     self.send_text(str(data));
   def on_close(self):
-    for idSource, idListener in self.events:
-      self.eventManager.disconnect(idSource, idListener);
+    for idSource, key in self.events:
+      self.eventManager.disconnect(idSource, key);
   # on_text and on_open keeps with the same behavior
 
 PORT=9001
